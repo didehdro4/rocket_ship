2ad4032e8b59f71bc66ac66d0cc96217a24c4230
didehdro4 <yangj079@newschool.edu>
2015-10-16 20:49:09 -0400

    Work In Class

--- rocket_ship_start/sketch.js
+++ rocket_ship_start/sketch.js
@@ -1,10 +1,52 @@
+var backgroundImage;
+var rocketImage;
+var standImage;
+var smokeeImage;
+var longSmokeImage;
+
+function preload(){
+	backgroundImage = loadImage("image/background.png")
+	rocketImage = loadImage("image/rocket.png")
+	standImage = loadImage("image/stand.png")
+	smokeeImage = loadImage("image/smokee.png")
+	longSmokeImage = loadImage("image/long smoke.png")
+}
+
 function setup() {
 	createCanvas(800, 600);
 }
 
 function draw() {
-	background(150, 200, 150);
-	fill(200, 255, 200);
+
+
+
+	image(backgroundImage, 0, 0);
+	image(standImage, 330, 450);
+
+	//find rockets possition
+	var rocketY = 280;
+	if (mouseIsPressed) {
+		rocketY = mouseY;
+		if(rocketY > 280){
+			rocketY = 280;
+		}
+	}
+	//draw smoke
+	if (mouseIsPressed){
+	image(smokeeImage, 345, 440);
+  image(longSmokeImage, 430, 150);
+}
+
 	noStroke();
-	ellipse(400, 300, 300, 300);
+	fill(145, 255, 255);
+	rectMode(CORNERS);
+	if(rocketY > 100){
+	rect(300, 100, 600, rocketY+120);
+
+	}
+
+ 	image(rocketImage, 410, rocketY);
+
+
+
 }
